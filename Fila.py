43ec95cae2201fd57332fd14f074ab81c56ea677
LuanRamalho <32373552+LuanRamalho@users.noreply.github.com>
2021-04-23 19:53:44 -0300

    Add files via upload

---
+++ Fila.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+class Fila():
+    def constrói():
+        z = ['d','dlask', 3, -692, 4928, 54, 'kdo432', 'vmewk', 93, -16]
+        
+        print(z)
+        print("")
+        z.append(87)
+        print("Enfileirando")
+        print(z)
+        print("")
+        z.append(-44)
+        print("Enfileirando")
+        print(z)
+        print("")
+        z.pop(0)
+        print("Desenfileirando")
+        print(z)
+        print("")
+        z.pop(0)
+        print("Desenfileirando")
+        print(z)
+
+f = Fila
+f.constrói()
+input()
